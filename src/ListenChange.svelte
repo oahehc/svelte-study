416e326d17364d821e57d9df90940e577a153399
Andrew_Chang <chehao.chang@smartnews.com>
2020-12-29 10:06:14 +0900

    listen variable

---
+++ src/ListenChange.svelte
@@ -0,0 +1,16 @@
+<script>
+  let count = 0
+  
+  const incrementCount = () => {
+    count = count + 1
+  }
+  
+  $: console.log(`${count}`)
+
+  $: {
+    console.log(`the count is ${count}`)
+    console.log(`double the count is ${count * 2}`)
+  }
+  </script>
+  
+  {count} <button on:click={incrementCount}>+1</button>
